15d3252ea1a291ee5c7e58021f0df3046195f292
Andrew Das <andrew.das@itaas.dimensiondata.com>
2016-06-02 12:15:31 -0400

    Generate file with csv extension

--- drprov.sh
+++ drprov.sh
@@ -3,9 +3,7 @@ echo "get servers for CG"
 didata tag list --tagKeyName=CG --tagKeyValue=$1 --query "ReturnKeys:Asset ID" | awk '{print $3}'  | sed '/^$/d' > working/cg-assets
 echo "get server info"
 x=0
-#echo "SERVER MAPPING FILE FOR CONSISTENCY GROUP $1 WITH PROD DATACENTER $2" > dr-server-mapping
-echo "*******************************************************" >> dr-server-mapping
-echo "Source_MCP,Source_VM_Name,Source_VM_IPV4,Source_VM_IPV6,Source_VM_OS,Source_VM_Displayname,Target_MCP,Target_VM_Name,Target_VM_IPV4,Target_VM_IPV6,Target_VM_OS,Target_VM_Displayname" > dr-server-mapping
+echo "Source_MCP,Source_VM_Name,Source_VM_IPV4,Source_VM_IPV6,Source_VM_OS,Source_VM_Displayname,Target_MCP,Target_VM_Name,Target_VM_IPV4,Target_VM_IPV6,Target_VM_OS,Target_VM_Displayname" > dr-server-mapping.csv
 
 while read p; do
   x=$((x+1))
@@ -13,25 +11,24 @@ while read p; do
   if [ $value == $2 ]
     then
         #get prod server info
-        #echo "PROD server" >> dr-server-mapping
+        #echo "PROD server" >> dr-server-mapping.csv
         sserver=$( didata --outputType=json server info --serverId=$p --query="ReturnKeys:ID,Name,Private IPv4 0,ipv6,OS_displayName,datacenterId" )
-	echo -n $value, >> dr-server-mapping
-         echo -n $sserver | jq .[0].ID | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping 
-	echo -n $sserver | jq '.[0]["Private IPv4 0"]' | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping
-        echo -n $sserver | jq .[0].ipv6 | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping
-	 echo -n $sserver | jq .[0].OS_displayName | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping
-	 echo -n $sserver | jq .[0].Name | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping
+	echo -n $value, >> dr-server-mapping.csv
+         echo -n $sserver | jq .[0].ID | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping.csv 
+	echo -n $sserver | jq '.[0]["Private IPv4 0"]' | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping.csv
+        echo -n $sserver | jq .[0].ipv6 | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping.csv
+	 echo -n $sserver | jq .[0].OS_displayName | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping.csv
+	 echo -n $sserver | jq .[0].Name | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping.csv
 
 	#get targer server info
         dr_target=$( didata tag list --assetId $p --tagKeyName target --query "ReturnKeys:Value" | awk '{print $2}' )        
 	dest=$( didata --outputType=json server info --serverId=$dr_target --query="ReturnKeys:ID,Name,Private IPv4 0,ipv6,OS_displayName,datacenterId" )
-        echo -n , >> dr-server-mapping
-	echo -n $dest | jq .[0].datacenterId | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping
-        echo -n $dest | jq .[0].ID | sed -e 's/^"//'  -e 's/"$//'  |tr '\n' ',' >> dr-server-mapping
-	 echo -n $dest | jq '.[0]["Private IPv4 0"]' | sed -e 's/^"//'  -e 's/"$//'|tr '\n' ','  >> dr-server-mapping
-	 echo -n $dest | jq .[0].ipv6 | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ','>> dr-server-mapping
-	 echo -n $dest | jq .[0].OS_displayName | sed -e 's/^"//'  -e 's/"$//'|tr '\n' ','  >> dr-server-mapping
-	 echo -n $dest | jq .[0].Name | sed -e 's/^"//'  -e 's/"$//' >> dr-server-mapping
+	echo -n $dest | jq .[0].datacenterId | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping.csv
+        echo -n $dest | jq .[0].ID | sed -e 's/^"//'  -e 's/"$//'  |tr '\n' ',' >> dr-server-mapping.csv
+	 echo -n $dest | jq '.[0]["Private IPv4 0"]' | sed -e 's/^"//'  -e 's/"$//'|tr '\n' ','  >> dr-server-mapping.csv
+	 echo -n $dest | jq .[0].ipv6 | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ','>> dr-server-mapping.csv
+	 echo -n $dest | jq .[0].OS_displayName | sed -e 's/^"//'  -e 's/"$//'|tr '\n' ','  >> dr-server-mapping.csv
+	 echo -n $dest | jq .[0].Name | sed -e 's/^"//'  -e 's/"$//' >> dr-server-mapping.csv
 
   fi
 done < working/cg-assets
