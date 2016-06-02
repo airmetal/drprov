bff38f79624bca0e2f5473716f7376fc45b4bec7
Andrew Das <andrew.das@itaas.dimensiondata.com>
2016-06-02 12:16:23 -0400

    Removed extra comments

--- drprov.sh
+++ drprov.sh
@@ -4,14 +4,12 @@ didata tag list --tagKeyName=CG --tagKeyValue=$1 --query "ReturnKeys:Asset ID" |
 echo "get server info"
 x=0
 echo "Source_MCP,Source_VM_Name,Source_VM_IPV4,Source_VM_IPV6,Source_VM_OS,Source_VM_Displayname,Target_MCP,Target_VM_Name,Target_VM_IPV4,Target_VM_IPV6,Target_VM_OS,Target_VM_Displayname" > dr-server-mapping.csv
-
 while read p; do
   x=$((x+1))
   value=$( didata server info --serverId=$p --query="ReturnKeys:datacenterId" | awk '{print $2}' )
   if [ $value == $2 ]
     then
         #get prod server info
-        #echo "PROD server" >> dr-server-mapping.csv
         sserver=$( didata --outputType=json server info --serverId=$p --query="ReturnKeys:ID,Name,Private IPv4 0,ipv6,OS_displayName,datacenterId" )
 	echo -n $value, >> dr-server-mapping.csv
          echo -n $sserver | jq .[0].ID | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ',' >> dr-server-mapping.csv 
@@ -29,7 +27,6 @@ while read p; do
 	 echo -n $dest | jq .[0].ipv6 | sed -e 's/^"//'  -e 's/"$//' |tr '\n' ','>> dr-server-mapping.csv
 	 echo -n $dest | jq .[0].OS_displayName | sed -e 's/^"//'  -e 's/"$//'|tr '\n' ','  >> dr-server-mapping.csv
 	 echo -n $dest | jq .[0].Name | sed -e 's/^"//'  -e 's/"$//' >> dr-server-mapping.csv
-
   fi
 done < working/cg-assets
 echo "procesed $x servers"
